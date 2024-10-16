1c59b96964743fda864d29eb3bb23d59da1d9f04
Gabriel Silva <gabrielferreirasilva04@outlook.com>
2024-10-16 12:56:41 -0300

    segundo commit

---
+++ src/main/java/com/github/gabrielferreiradasilva04/produtosapi/model/Produto.java
@@ -0,0 +1,12 @@
+package com.github.gabrielferreiradasilva04.produtosapi.model;
+
+
+public class Produto {
+	private String id;
+	private String nome;
+	private String descricao;
+	private Double preco;
+
+	
+	
+}
