25e7b8b7d18590e47bd9773dc8b9c1efa79763dc
Gabriel Silva <gabrielferreirasilva04@outlook.com>
2024-10-17 01:09:00 -0300

    inicio do terceiro modulo de entendimento do spring framework

--- src/main/java/com/github/gabrielferreiradasilva04/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/github/gabrielferreiradasilva04/produtosapi/controller/ProdutoController.java
@@ -1,13 +1,16 @@
 package com.github.gabrielferreiradasilva04.produtosapi.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.gabrielferreiradasilva04.produtosapi.model.Produto;
@@ -23,11 +26,9 @@ public class ProdutoController {
 		this.repository = produtoRepository;
 	}
 	
-	@PostMapping /*nas versões novas do spring não precisa especificar que o método retornará algo, basta
-	retornar a entidade que ele entende que deve retornar algo em formato de JSON, mas esse formato pode ser alterado*/
+	@PostMapping 
 	public Produto salvar(@RequestBody Produto produto) {
-		System.out.println("Produto recebido: "+produto);
-		var produtoID = UUID.randomUUID().toString();
+		var produtoID = UUID.randomUUID().toString(); 
 		produto.setId(produtoID);
 		repository.save(produto);
 		return produto;
@@ -43,4 +44,17 @@ public class ProdutoController {
 	public void deletar(@PathVariable String id) {
 		repository.deleteById(id);
 	}
+	
+	
+	@PutMapping("{id}")
+	public void atualizar(@PathVariable String id, @RequestBody Produto produto) {
+		produto.setId(id);
+		repository.save(produto);
+	}
+	
+	@GetMapping
+	public List<Produto> buscar(@RequestParam String nome){
+		return repository.findByNome(nome);
+	}
+
 }
--- src/main/java/com/github/gabrielferreiradasilva04/produtosapi/repository/ProdutoRepository.java
+++ src/main/java/com/github/gabrielferreiradasilva04/produtosapi/repository/ProdutoRepository.java
@@ -1,5 +1,7 @@
 package com.github.gabrielferreiradasilva04.produtosapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.gabrielferreiradasilva04.produtosapi.model.Produto;
@@ -7,4 +9,5 @@ import com.github.gabrielferreiradasilva04.produtosapi.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
 
+	List<Produto> findByNome(String nome);
 }
